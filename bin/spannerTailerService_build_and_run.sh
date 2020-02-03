a756ba79dd8529b7836db31e8d903fd541c206e1
Chris Keating <pdex@google.com>
2020-02-03 23:28:02 +0000

    Reduce headaches with the docker script.
    * Give the container a name
    * Kill the named container if it's running (might be overkill)
    * Autoremove the container when it's done running

--- bin/spannerTailerService_build_and_run.sh
+++ bin/spannerTailerService_build_and_run.sh
@@ -2,4 +2,9 @@
 
 set -ef
 
-docker build -t spez/spanner-tailer-service -f cdc/docker/Dockerfile.SpannerTailerService . && docker run -p 9010:9010 -t spez/spanner-tailer-service:latest
+CONTAINER_NAME=spanner-tailer
+
+docker container kill $CONTAINER_NAME || echo
+
+docker build -t spez/spanner-tailer-service -f cdc/docker/Dockerfile.SpannerTailerService . && \
+	docker run --name $CONTAINER_NAME -p 9010:9010 -t --rm spez/spanner-tailer-service:latest

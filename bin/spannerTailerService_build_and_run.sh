8b83f6f8a574adc57382a66e1b1a3a891903440d
Chris Keating <pdex@google.com>
2020-02-04 18:16:04 +0000

    Don't output an error if the container is dead.

--- bin/spannerTailerService_build_and_run.sh
+++ bin/spannerTailerService_build_and_run.sh
@@ -4,7 +4,7 @@ set -ef
 
 CONTAINER_NAME=spanner-tailer
 
-docker container kill $CONTAINER_NAME || echo
+docker container kill $CONTAINER_NAME > /dev/null 2>&1 || echo
 
 docker build -t spez/spanner-tailer-service -f cdc/docker/Dockerfile.SpannerTailerService . && \
 	docker run --name $CONTAINER_NAME -p 9010:9010 -t --rm spez/spanner-tailer-service:latest

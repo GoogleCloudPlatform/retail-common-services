91408c2c59b8ed0a6d4a0a26e997c1c4f19c0020
Chris Keating <pdex@google.com>
2020-02-10 19:23:00 +0000

    Lots of docker build tweaks
    * Build both a dev and prod image
      * dev image uses fat jdk and debian for debugging
      * prod image uses distroless jdk
    * Build and run script can optionally skip the build
    * Setup runtime parameters for running jmc over X11
    * Break out build-env into it's own Dockerfile
      * This way we can force docker to cache base image
    * dev image installs gtk libs needed for jmc
    * Fixed wrong RMI port in jvm-arguments
    * Updated -XX: flags in jvm-arguments

--- bin/spannerTailerService_build_and_run.sh
+++ bin/spannerTailerService_build_and_run.sh
@@ -6,5 +6,27 @@ CONTAINER_NAME=spanner-tailer
 
 docker container kill $CONTAINER_NAME > /dev/null 2>&1 || echo
 
-docker build -t spez/spanner-tailer-service -f cdc/docker/Dockerfile.SpannerTailerService . && \
-	docker run --name $CONTAINER_NAME -p 9010:9010 -t --rm spez/spanner-tailer-service:latest
+if [[ "$1" = "prod" ]]; then
+  IMAGE=spez/spanner-tailer-service:latest
+  shift
+else
+  IMAGE=spez/spanner-tailer-service-dev:latest
+fi
+
+if [[ "$1" = "nobuild" ]]; then
+  BUILD="n"
+else
+  BUILD="y"
+fi
+
+XSOCK=/tmp/.X11-unix
+JMC=$PWD/../jmc
+DOCKERFILE="-f cdc/docker/Dockerfile.SpannerTailerService"
+BASEFILE="-f cdc/docker/Dockerfile.build-env"
+set -x
+if [[ "$BUILD" = "y" ]]; then
+  docker build --target build-env -t spez/build-env $BASEFILE .
+  docker build --target dev       -t spez/spanner-tailer-service-dev $DOCKERFILE .
+  docker build --target prod      -t spez/spanner-tailer-service $DOCKERFILE .
+fi
+docker run --name $CONTAINER_NAME -v $XSOCK:$XSOCK -v $JMC:/app/jmc -p 9010:9010 -t --rm $IMAGE

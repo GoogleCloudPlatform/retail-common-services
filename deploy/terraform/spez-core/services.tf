16d93f67059139046d3f6eeffe6a55a74e3085a5
Chris Keating <pdex@google.com>
2021-11-24 17:25:40 -0600

    removed unused shell scripts

--- deploy/terraform/spez-core/services.tf
+++ deploy/terraform/spez-core/services.tf
@@ -16,40 +16,50 @@
 
 resource "google_project_service" "cloudbuild" {
   service = "cloudbuild.googleapis.com"
+  disable_dependent_services = true
 }
 
 resource "google_project_service" "cloudfunctions" {
   service = "cloudfunctions.googleapis.com"
+  disable_dependent_services = true
 }
 
 resource "google_project_service" "cloudresourcemanager" {
   service = "cloudresourcemanager.googleapis.com"
+  disable_dependent_services = true
 }
 
 resource "google_project_service" "compute" {
   service = "compute.googleapis.com"
+  disable_dependent_services = true
 }
 
 resource "google_project_service" "container" {
   service = "container.googleapis.com"
+  disable_dependent_services = true
 }
 
 resource "google_project_service" "containerregistry" {
   service = "containerregistry.googleapis.com"
+  disable_dependent_services = true
 }
 
 resource "google_project_service" "iam" {
   service = "iam.googleapis.com"
+  disable_dependent_services = true
 }
 
 resource "google_project_service" "pubsub" {
   service = "pubsub.googleapis.com"
+  disable_dependent_services = true
 }
 
 resource "google_project_service" "spanner" {
   service = "spanner.googleapis.com"
+  disable_dependent_services = true
 }
 
 resource "google_project_service" "storage-component" {
   service = "storage-component.googleapis.com"
+  disable_dependent_services = true
 }

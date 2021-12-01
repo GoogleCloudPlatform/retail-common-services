2c4781988903113c1c5ab2e25a285d19287ecf7b
Chris Keating <pdex@google.com>
2021-12-01 15:14:11 -0600

    typo

--- deploy/terraform/spez-example/tailer.tf
+++ deploy/terraform/spez-example/tailer.tf
@@ -32,9 +32,9 @@ data "google_container_cluster" "primary" {
 }
 
 provider "kubernetes" {
-  host = data.google_container_cluster.spez-tailer-cluster.endpoint
+  host = data.google_container_cluster.primary.endpoint
   token = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.spez-tailer-cluster.master_auth[0].cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
 }
 
 data "google_client_config" "default" {}

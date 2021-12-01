a8c02e1faeef5a6580aac4fc979086c72458ffb3
Chris Keating <pdex@google.com>
2021-12-01 15:30:11 -0600

    fixed host url

--- deploy/terraform/spez-example/tailer.tf
+++ deploy/terraform/spez-example/tailer.tf
@@ -32,7 +32,7 @@ data "google_container_cluster" "primary" {
 }
 
 provider "kubernetes" {
-  host = data.google_container_cluster.primary.endpoint
+  host  = "https://${data.google_container_cluster.primary.endpoint}"
   token = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
 }

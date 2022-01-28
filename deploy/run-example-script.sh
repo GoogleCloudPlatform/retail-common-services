b8fb0093b7de399850ddb0a179f6e88b0d735446
xjdr <jeff.rose12@gmail.com>
2022-01-28 20:51:19 +0000

    cleaned up example script names

--- deploy/run-example-script.sh
+++ deploy/run-example-script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+pushd example-script
 project_id=$1
-docker build -t rcs-run-example .
+docker build -t rcs-example-script:latest .
 docker run -v "$HOME/.config/gcloud:/gcp/config:ro" \
   --env CLOUDSDK_CONFIG=/gcp/config \
   --env GOOGLE_APPLICATION_CREDENTIALS=/gcp/config/application_default_credentials.json \
   --env GCLOUD_PROJECT=rcs-demo-prod \
-  -it rcs-run-example $project_id
+  -it rcs-example-script:latest $project_id
+popd

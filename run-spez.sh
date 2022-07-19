b242c6749ac54f428ee9ffc8bfa2de64485544f3
xjdr <jeff.rose12@gmail.com>
2022-07-19 22:42:38 +0000

    helper script for local

---
+++ run-spez.sh
@@ -0,0 +1,12 @@
+export PROJECT_ID = "retail-common-services-249016"
+export SINK_INSTANCE = ""
+export SINK_DATABASE = ""
+export SINK_TABLE = ""
+export LPTS_INSTANCE = ""
+export LPTS_DATABASE = ""
+export LPTS_TABLE = ""
+export DEFAULT_LOG_LEVEL = ""
+export TIMESTAMP_COLUMN = ""
+export UUID_COLUMN = ""
+
+./gradlew run
